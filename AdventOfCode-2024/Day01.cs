84256c6c12b5166d7a6b0ae46fb07d8a5420ff54
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-01-17 16:08:53 +0100

    Create Project & Finish Day 01 Exercise 1

---
+++ AdventOfCode-2024/Day01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2024
+{
+    internal class Day01
+    {
+        private List<String> puzzleInput;
+
+        public Day01()
+        {
+            GetPuzzleInput();
+        }
+
+        private void GetPuzzleInput()
+        {
+            puzzleInput = File
+                .ReadLines(Environment.GetEnvironmentVariable("D01_P1_FILE_PATH"))
+                .ToList()
+                ;
+        }
+
+        public int SumCalibrationValues()
+        {
+            return puzzleInput.Select(s => int.Parse(s.First(c => char.IsDigit(c)).ToString() + s.Last(c => char.IsDigit(c)).ToString())).Sum();
+        }
+
+    }
+}
