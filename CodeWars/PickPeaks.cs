1ce1a06814d2f6931939334363ef7e7a2647f0e9
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-01-30 10:03:02 +0100

    Add 3rd working solution that passes tests

--- CodeWars/PickPeaks.cs
+++ CodeWars/PickPeaks.cs
@@ -10,51 +10,37 @@ namespace CodeWars
     {
         public static Dictionary<string, List<int>> GetPeaks(int[] arr)
         {
-            var slopes = new int[arr.Length - 1];
             var output = new Dictionary<string, List<int>>() { { "pos", new List<int>() }, { "peaks", new List<int>() } };
 
-            for (int i = 0; i < slopes.Length; i++)
+            for (int i = 1; i < arr.Length - 1; i++)
             {
-                slopes[i] = arr[i + 1] - arr[i];
-            }
-
-            for (int i = 0; i < slopes.Length - 1; i++)
-            {
-                if (slopes[i] > 0 && slopes[i + 1] < 0)
+                if (arr[i] - arr[i - 1] > 0 && arr[i + 1] - arr[i] < 0)
                 {
-                    output["pos"].Add(i + 1);
-                    output["peaks"].Add(arr[i + 1]);
+                    output["pos"].Add(i);
+                    output["peaks"].Add(arr[i]);
                 }
-                else if (slopes[i] == 0)
+                else if (arr[i] - arr[i - 1] > 0 && arr[i + 1] - arr[i] == 0)
                 {
-                    var result = IsPlateauWithEnd(i, slopes);
-                    if (result.Item1)
+                    var plateauEnds = false;
+                    for (int j = i + 1; j < arr.Length - 1; j++)
+                    {
+                        if (arr[j] - arr[j - 1] == 0 && arr[j + 1] - arr[j] != 0)
+                        {
+                            if(arr[j + 1] - arr[j] < 0)
+                            {
+                                plateauEnds = true;
+                            }
+                            break;
+                        }
+                    }
+                    if (plateauEnds)
                     {
                         output["pos"].Add(i);
                         output["peaks"].Add(arr[i]);
                     }
-                    i = result.Item2 - 1;
                 }
             }
             return output;
-        }
-
-        private static (bool, int) IsPlateauWithEnd(int startingIndex, int[] slopes)
-        {
-            var endingIndex = startingIndex;
-            for (int i = startingIndex + 1; i < slopes.Length; i++)
-            {
-                endingIndex = i;
-                if (slopes[i] < 0)
-                {
-                    return (true, endingIndex);
-                }
-                if (slopes[i] > 0)
-                {
-                    return (false, endingIndex);
-                }
-            }
-            return (false, endingIndex);
         }
     }
 }
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodeWars;
 
-PickPeaks.GetPeaks(new int[] { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1 });
+PickPeaks.GetPeaks(new int[] { 1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3});
 //GreedIsGood.Score(new int[] { 1, 1, 1, 2, 1 });
