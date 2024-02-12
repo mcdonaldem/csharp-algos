2f591ed042d2f22b4791b830d8f770f4f08389bc
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-02-12 09:15:15 +0100

    First working solution

---
+++ CodeWars/FindTheSmallest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    internal class FindTheSmallest
+    {
+        public static long[] Smallest(long n)
+        {
+            var output = new long[3] {n, 0, 0};
+            for (int i = 0; i < n.ToString().Length; i++)
+            {
+                for (int j = 0; j < n.ToString().Length; j++)
+                {
+                    var nDigits = new StringBuilder(n.ToString());
+                    var digit = nDigits[i];
+                    nDigits.Remove(i, 1);
+                    nDigits.Insert(j, digit);
+                    if(long.Parse(nDigits.ToString()) < output[0])
+                    {
+                        output = [long.Parse(nDigits.ToString()), i, j];
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodeWars;
 
-Balanced.BalancedParens(1);
+Console.WriteLine(FindTheSmallest.Smallest(209917));
+//Balanced.BalancedParens(1);
 //PickPeaks.GetPeaks(new int[] { 1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3});
 //GreedIsGood.Score(new int[] { 1, 1, 1, 2, 1 });
