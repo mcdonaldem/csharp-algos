587e36e7405a8aa3b0bb4ba4123cb8b4417bffbd
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-02-12 10:32:22 +0100

    First attempt - non-working solution

---
+++ CodeWars/BurrowsWheelerTransformation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    internal class BurrowsWheelerTransformation
+    {
+        public static Tuple<string, int> Encode(string s)
+        {
+            return null;
+        }
+
+        public static string Decode(string s, int i)
+        {
+            var output = "";
+            var firstColumn = s.Order().ToArray();
+            var indexVisited = new bool[firstColumn.Length];
+            for (int j = 0; j < s.Length; j++)
+            {
+                output = s[i] + output;
+                indexVisited[i] = true;
+                i = firstColumn
+                    .Select((c, k) => new { Char = c, Index = k })
+                    .Last(a => a.Char.Equals(s[i]) && !indexVisited[a.Index])
+                    .Index;
+            }
+            return output;
+        }
+    }
+}
