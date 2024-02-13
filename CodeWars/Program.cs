bc567395c4327b41a3076cff9f4898cbebb46a64
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-02-13 10:24:00 +0100

    Add final working solution

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodeWars;
 
-Console.WriteLine(FindTheSmallest.Smallest(209917));
+SumByFactors.sumOfDivided(new int[] { 125, 103, -76, 141, 192, -56, 130, 147, -53, 178, 130, 107, 192, 53, 196 });
+//BurrowsWheelerTransformation.Encode("");
+//BurrowsWheelerTransformation.Decode("e emnllbduuHB", 2);
+//Console.WriteLine(FindTheSmallest.Smallest(209917));
 //Balanced.BalancedParens(1);
 //PickPeaks.GetPeaks(new int[] { 1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3});
 //GreedIsGood.Score(new int[] { 1, 1, 1, 2, 1 });
---
+++ CodeWars/SumByFactors.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    internal class SumByFactors
+    {
+        public static string sumOfDivided(int[] lst)
+        {
+            var primes = new Dictionary<int, int>();
+            for (int i = 0; i < lst.Length; i++)
+            {
+                var iPrimes = GetPrimeFactors(Math.Abs(lst[i]));
+                foreach (var p in iPrimes)
+                {
+                    if (!primes.TryAdd(p, lst[i]))
+                    {
+                        primes[p] += lst[i];
+                    }
+                }
+            }
+            return String.Join("", primes.OrderBy(kvp => kvp.Key).Select(kvp => $"({kvp.Key} {kvp.Value})"));
+        }
+
+        private static HashSet<int> GetPrimeFactors(int n)
+        {
+            var originalN = n;
+            var primes = new HashSet<int>();
+            // Check for 2
+            while (n % 2 == 0)
+            {
+                primes.Add(2);
+                n /= 2;
+            }
+            for (int i = 3; i <= Math.Sqrt(originalN); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    primes.Add(i);
+                    n /= i;
+                }
+                if(n < i)
+                {
+                    break;
+                }
+            }
+            if (n > 1)
+            {
+                primes.Add(n);
+            }
+            return primes;
+        }
+    }
+}
