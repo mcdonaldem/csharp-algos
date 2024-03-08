8727127efd6ae893e27d43ac2ed3df2d2cf0537c
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-08 17:03:21 +0100

    Add accepted Merge Sorted Array solution

---
+++ LeetCode/MergeSortedArray88.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class MergeSortedArray88
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m + n - 1; i >= 0; i--)
+            {
+                if (n == 0) break;
+                var is1bigger = nums1[m - 1 < 0 ? 0 : m - 1] > nums2[n - 1];
+                if (is1bigger)
+                {
+                    nums1[i] = nums1[m - 1];
+                    nums1[m - 1] = 0;
+                    m--;
+                }
+                else
+                {
+                    nums1[i] = nums2[n - 1];
+                    n--;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+
+var msa = new MergeSortedArray88();
+msa.Merge([0, 0, 3, 0, 0, 0, 0, 0, 0], 3, [-1, 1, 1, 1, 2, 3], 6);
